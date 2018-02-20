712cf7ca5e4edbb8b71397a6706ca7cbf5cf22f0
D George <dgeorge@Ds-MacBook-Air.local>
2018-02-19 22:23:49 -0800

    split up portfolio into sections

--- header.php
+++ header.php
@@ -5,11 +5,10 @@
 		<title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' :'; } ?> <?php bloginfo('name'); ?></title>
 
 		<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/flexboxgrid/6.3.1/flexboxgrid.min.css" type="text/css" >
-
-
+		<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
 		<link href="//www.google-analytics.com" rel="dns-prefetch">
-        <link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
-        <link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
+    <link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
+    <link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
 
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -24,7 +23,15 @@
             tests: {}
         });
         </script>
-
+				<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+				<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+				<script>
+				  $( function() {
+				    $( "#accordion" ).accordion({
+				      heightStyle: "content"
+				    });
+				  } );
+				  </script>
 	</head>
 	<body <?php body_class(); ?>
 
--- template-d-w-i-g-h-t-hompage.php
+++ template-d-w-i-g-h-t-hompage.php
@@ -5,10 +5,17 @@
 		<div class="row center-xs">
 			<div class="col-md-12">
 				<div class="box">
-						<img id="home_neopolitan_svg" style="height:300px;" alt="icecream cone" src="<?php echo get_template_directory_uri() . '/img/neopolitan.svg'; ?>" />
+						<a href="http://d-w-i-g-h-t.com/portfolio"><img id="home_neopolitan_svg" style="height:300px;" alt="icecream cone" src="<?php echo get_template_directory_uri() . '/img/neopolitan.svg'; ?>" /></a>
 				</div>
 			</div>
 		</div>
+		<br class="clear">
+		<br class="clear">
+
+		<h3 style="text-align: center;"><a href="http://d-w-i-g-h-t.com/wp-content/themes/html5blank-stable/img/DwightJohnson-d-w-i-g-h-t.com-Resume-2018.pdf">Print Résumé</a> | <a href="http://d-w-i-g-h-t.com/portfolio">View Portfolio</a></h3>
+
+		<br class="clear">
+		<br class="clear">
 
 	</main>
 
